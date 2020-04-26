7efa3c7957a76711180e442ef53c165325872c21
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2020-04-27 00:54:16 +0530

    setting up exoplayer for media streaming

---
+++ player-lib/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package com.example.exoplayer
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
+class PlayerActivity : AppCompatActivity() {
+    lateinit var playerView:PlayerView
+    var mPlayer: SimpleExoPlayer? = null
+    var playWhenReady = true
+    var currentWindow: Int = 0
+    var playBackPosition: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+
+        playerView = findViewById(R.id.video_view)
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val ds: DataSource.Factory = DefaultDataSourceFactory(this, "exoplayer-codelab")
+        return ProgressiveMediaSource.Factory(ds).createMediaSource(uri)
+    }
+
+    private fun initializePlayer() {
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+        playerView.player = mPlayer
+        val uri = Uri.parse(getString(R.string.media_url_mp4))
+        val mediaSource = buildMediaSource(uri)
+        mPlayer?.playWhenReady = playWhenReady
+        mPlayer?.seekTo(currentWindow, playBackPosition)
+        mPlayer?.prepare(mediaSource, false, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24 )
+            initializePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24)
+            releasePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT < 24 && mPlayer == null) {
+            initializePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >=24)
+            releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        if (mPlayer != null) {
+            playWhenReady = mPlayer!!.playWhenReady
+            playBackPosition = mPlayer!!.currentPosition
+            currentWindow = mPlayer!!.currentWindowIndex
+            mPlayer!!.release()
+            mPlayer = null
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+}
